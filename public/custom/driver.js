d7e7f9c7f46cf3db977cc97ad39a22a412648ffb
“['Shueb <mohitsainisaini2680@gmail.com>
2025-07-13 15:24:10 +0530

    fixing the bugs

--- public/custom/driver.js
+++ public/custom/driver.js
@@ -47,7 +47,13 @@ function connectionDenied(message) {
   }
 }
 
-function areLatLonClose(lat1, lon1, lat2, lon2, tolerance = 0.000015) {
+function areLatLonClose(
+  lat1,
+  lon1,
+  lat2,
+  lon2,
+  tolerance = (tolerance = 0.000009)
+) {
   return Math.abs(lat1 - lat2) < tolerance && Math.abs(lon1 - lon2) < tolerance;
 }
 
